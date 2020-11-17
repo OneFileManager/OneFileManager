3949b39819cbc18af456f450acd652201e849157
hyfree-msi <wangxianqiangcn@foxmail.com>
2020-11-17 23:13:20 +0800

    增加文件夹图标

--- OneFileManager/CustomUserControl/Main/FileListControl.xaml.cs
+++ OneFileManager/CustomUserControl/Main/FileListControl.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using OneFileManager.Model;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Text;
 using System.Windows;
@@ -26,27 +27,51 @@ namespace OneFileManager.CustomUserControl.Main
         }
 
 
-        List<FileInfo> fileList = new List<FileInfo>();
+        ObservableCollection<FileListViewNode> fileList = new ObservableCollection<FileListViewNode>();
         public void ShowFilesList(string path)
         {
-            var files = Directory.GetFiles(path);
-            
+         
+            //清除视图
             fileList.Clear();
+            string[] dirs= Directory.GetDirectories(path);
+
+            foreach (var item in dirs)
+            {
+                var dirInfo=new DirectoryInfo(item);
+                var flvm=new FileListViewNode(dirInfo);
+                fileList.Add(flvm);
+            }
+
+            var files = Directory.GetFiles(path);
             foreach (var item in files)
             {
                 var fileinfo=new FileInfo(item);
-                var fileModel=new FileModel(fileinfo.Name);
-                fileList.Add(fileinfo);
-                Console.WriteLine(  fileinfo.Name);
-
+                 var flm=new FileListViewNode(fileinfo);
+                fileList.Add(flm);
             }
-            lvwFiles.ItemsSource= fileList;
-
+     
         }
 
         private void UserControl_Initialized(object sender, EventArgs e)
         {
-            ShowFilesList(@"C:\Windows\System32");
+            lvwFiles.ItemsSource = fileList;
+            ShowFilesList(@"D:\");
+        }
+
+        private void lvwFiles_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var file=lvwFiles.SelectedItem as FileListViewNode;
+            switch (file.FileType)
+            {
+                case FileListViewNode.FileListViewNodeType.File:
+                    break;
+                case FileListViewNode.FileListViewNodeType.Directory:
+                    ShowFilesList(file.FullName);
+                    break;
+                default:
+                    break;
+            }
+
         }
     }
 }
---
+++ OneFileManager/Model/FileListViewNode.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneFileManager.Model
+{
+    public class FileListViewNode
+    {
+        public FileListViewNodeType FileType { get;set;}
+        public string ImagePath { get;set;}
+        public string Name { get;set;}
+        public string FullName { get; set; }
+        public string LastWriteTime { get;}
+        public string Extension { get;}
+        public string Length { get;}
+
+     
+        public enum FileListViewNodeType
+        {
+            File, Directory
+        }
+        public FileListViewNode(FileInfo fileInfo)
+        {
+            this.ImagePath="/Resources/UI/Default/File.png";
+            this.FileType= FileListViewNodeType.File;
+            this.Name=fileInfo.Name;
+            this.FullName = fileInfo.FullName;
+            this.LastWriteTime = fileInfo.LastWriteTime.ToString("G");
+            this.Extension = fileInfo.Extension;
+            this.Length = fileInfo.Length+"字节";
+        }
+        public FileListViewNode(DirectoryInfo directoryInfo)
+        {
+            this.ImagePath = "/Resources/UI/Default/FolderOrange.png";
+            this.FileType = FileListViewNodeType.Directory;
+            
+            this.Name = directoryInfo.Name;
+            this.FullName = directoryInfo.FullName;
+            this.LastWriteTime = directoryInfo.LastWriteTime.ToString("G");
+            this.Extension = "文件夹";
+            this.Length = "未知";
+        }
+    }
+}
